cf4322230aa37fa2708468a9d33e957854df06b9
Shuai <sz3034@columbia.edu>
2022-09-26 21:21:50 +0000

    main loop finished

--- Assignments/Assignment1/E4750.2022Fall.sz3034.assignment1.PyCUDA.py
+++ Assignments/Assignment1/E4750.2022Fall.sz3034.assignment1.PyCUDA.py
@@ -110,7 +110,7 @@ class CudaModule:
         cuda.memcpy_dtoh(c, c_gpu)
 
         # return a tuple of output of addition and time taken to execute the operation.
-        return (c, start.time_till(end))
+        return (c, start.time_till(end) * 1000) # in us
 
     def add_host_mem_gpu(self, a, b, length, is_b_a_vector):
         """
@@ -178,7 +178,7 @@ class CudaModule:
         end.synchronize()
 
         # return a tuple of output of addition and time taken to execute the operation.
-        return (c, start.time_till(end))
+        return (c, start.time_till(end) * 1000) # in us
 
     def add_gpuarray_no_kernel(self, a, b, length, is_b_a_vector):
         """
@@ -224,7 +224,7 @@ class CudaModule:
         c = c_gpu.get()
         
         # return a tuple of output of addition and time taken to execute the operation.
-        return (c, start.time_till(end))
+        return (c, start.time_till(end) * 1000) # in us
         
     def add_gpuarray(self, a, b, length, is_b_a_vector):
         """
@@ -285,7 +285,7 @@ class CudaModule:
         c = c_gpu.get()
         
         # return a tuple of output of addition and time taken to execute the operation.
-        return (c, start.time_till(end))
+        return (c, start.time_till(end) * 1000) # in us
 
     def CPU_Add(self, a, b, length, is_b_a_vector):
         """
@@ -300,7 +300,7 @@ class CudaModule:
         c = a + b
         end = time.time()
 
-        return c, end - start
+        return c, (end - start) * 1e6 # return in us
 
     def CPU_Loop_Add(self, a, b, length, is_b_a_vector):
         """
@@ -321,7 +321,7 @@ class CudaModule:
                 c[index] = a[index] + b
         end = time.time()
 
-        return c, end - start
+        return c, (end - start) * 1e6 # return in us
 
 def main():
     # List all main methods
@@ -342,8 +342,7 @@ def main():
     valid_main_methods = all_main_methods
 
     # Select the list of valid vector_sizes for current_analysis
-    # valid_vector_sizes = vector_sizes[0:6]
-    valid_vector_sizes = vector_sizes[0:5]
+    valid_vector_sizes = vector_sizes[0:6]
 
     # Create an instance of the CudaModule class
     graphicscomputer = CudaModule()
@@ -354,26 +353,43 @@ def main():
     # The third loop runs 50 repetitions, for each case of the second loop
     # The fourth loop iterates between the different CPU/GPU/Memory-transfer methods, for each case of the third loop.
 
+    # loop through vector or scaler operarion
     for current_operation in valid_operations:
         arr_avg_total_cpu_time = np.array([])
         arr_avg_total_cpu_loop_time = np.array([])
         # [TODO: Students should write Code]
         # Add for the rest of the methods
+        arr_avg_total_add_device_mem_gpu_time = np.array([])
+        arr_avg_total_add_host_mem_gpu_time = np.array([])
+        arr_avg_total_add_gpuarray_no_kernel_time = np.array([])
+        arr_avg_total_add_gpuarray_using_kernel_time = np.array([])
+
+        # loop throuh size = 10^0 - 10^6
         for vector_size in valid_vector_sizes:
 
             arr_total_cpu_time = np.array([])
             arr_total_cpu_loop_time = np.array([])
             # [TODO: Students should write Code]
             # Add for the rest of the methods
+            arr_total_add_device_mem_gpu_time = np.array([])
+            arr_total_add_host_mem_gpu_time = np.array([])
+            arr_total_add_gpuarray_no_kernel_time = np.array([])
+            arr_total_add_gpuarray_using_kernel_time = np.array([])
 
             print ("vectorlength")
             print (vector_size)
 
             a_array_np = np.arange(1,vector_size+1).astype(np.float32)
-            b = 3 # Choose any number you desire
+            b = 39 # my lucky number
             b_number_np = np.float32(b) # storing as number having value b with datatype Float32
             b_array_np = b*np.ones(vector_size).astype(np.float32) # storing as array with all elements having equal value b as datatype Float32
+            
+
+            # loop through 50 round
             for iteration in iteration_indexes:
+                
+                
+                # loop through all 6 methord
                 for current_method in valid_main_methods:
                     if(current_operation == 'Pass Vector and Number'):
                         is_b_a_vector = False
@@ -381,26 +397,83 @@ def main():
                     else:
                         is_b_a_vector = True
                         b_in = b_array_np
+
                     if(current_method == 'CPU Add'): # baseline
                         c_np_cpu_add, cpu_time_add = graphicscomputer.CPU_Add(a_array_np,b_in,vector_size,is_b_a_vector)
                         arr_total_cpu_time = np.append(arr_total_cpu_time, cpu_time_add)
+
                     if(current_method == 'CPU_Loop_Add'):
                         c_np_cpu_loop_add, cpu_time_loop_add = graphicscomputer.CPU_Loop_Add(a_array_np,b_in,vector_size,is_b_a_vector)
-                        sum_diff = c_np_cpu_loop_add - c_np_cpu_add
                         arr_total_cpu_loop_time = np.append(arr_total_cpu_loop_time, cpu_time_loop_add)
-                        total_diff = sum_diff.sum()
                         # check correctness
+                        sum_diff = c_np_cpu_loop_add - c_np_cpu_add
+                        total_diff = sum_diff.sum()
                         if (total_diff != 0):
                             print (current_method + " " + current_operation + "sum mismatch")
                             print (total_diff)
                     # [TODO: Students should write Code]
+                    
+                    if (current_method == 'add_device_mem_gpu'):
+                        add_device_mem_gpu_result, add_device_mem_gpu_time  = graphicscomputer.add_device_mem_gpu(a_array_np,b_in,vector_size,is_b_a_vector)
+                        arr_total_add_device_mem_gpu_time = np.append(arr_total_add_device_mem_gpu_time, add_device_mem_gpu_time)
+                        # check correctness
+                        sum_diff = add_device_mem_gpu_result - c_np_cpu_add
+                        total_diff = sum_diff.sum()
+                        if (total_diff != 0):
+                            print (current_method + " " + current_operation + "sum mismatch")
+                            print (total_diff)
+                    
+                    if (current_method == 'add_host_mem_gpu'):
+                        add_host_mem_gpu_result, add_host_mem_gpu_time  = graphicscomputer.add_host_mem_gpu(a_array_np,b_in,vector_size,is_b_a_vector)
+                        arr_total_add_host_mem_gpu_time = np.append(arr_total_add_host_mem_gpu_time, add_host_mem_gpu_time)
+                        # check correctness
+                        sum_diff = add_host_mem_gpu_result - c_np_cpu_add
+                        total_diff = sum_diff.sum()
+                        if (total_diff != 0):
+                            print (current_method + " " + current_operation + "sum mismatch")
+                            print (total_diff)
+                    
+                    if (current_method == 'add_gpuarray_no_kernel'):
+                        add_gpuarray_no_kernel_result, add_gpuarray_no_kernel_time  = graphicscomputer.add_gpuarray_no_kernel(a_array_np,b_in,vector_size,is_b_a_vector)
+                        arr_total_add_gpuarray_no_kernel_time = np.append(arr_total_add_gpuarray_no_kernel_time, add_gpuarray_no_kernel_time)
+                        # check correctness
+                        sum_diff = add_gpuarray_no_kernel_result - c_np_cpu_add
+                        total_diff = sum_diff.sum()
+                        if (total_diff != 0):
+                            print (current_method + " " + current_operation + "sum mismatch")
+                            print (total_diff)
+                    
+                    if (current_method == 'add_gpuarray_using_kernel'):
+                        add_gpuarray_using_kernel_result, add_gpuarray_using_kernel_time  = graphicscomputer.add_gpuarray(a_array_np,b_in,vector_size,is_b_a_vector)
+                        arr_total_add_gpuarray_using_kernel_time = np.append(arr_total_add_gpuarray_using_kernel_time, add_gpuarray_using_kernel_time)
+                        # check correctness
+                        sum_diff = add_gpuarray_using_kernel_result - c_np_cpu_add
+                        total_diff = sum_diff.sum()
+                        if (total_diff != 0):
+                            print (current_method + " " + current_operation + "sum mismatch")
+                            print (total_diff)
+                    
+                    
                     # Add for the rest of the methods
             avg_total_cpu_time = ((arr_total_cpu_time.sum())/50)
             arr_avg_total_cpu_time = np.append(arr_avg_total_cpu_time, avg_total_cpu_time)
+
             avg_total_cpu_loop_time = ((arr_total_cpu_loop_time.sum())/50)
             arr_avg_total_cpu_loop_time = np.append(arr_avg_total_cpu_loop_time, avg_total_cpu_loop_time)
 
             # [TODO: Students should write Code]
+            avg_total_add_device_mem_gpu_time = ((arr_total_add_device_mem_gpu_time.sum())/50)
+            arr_avg_total_add_device_mem_gpu_time = np.append(arr_avg_total_add_device_mem_gpu_time, avg_total_add_device_mem_gpu_time)
+            
+            avg_total_add_host_mem_gpu_time = ((arr_total_add_host_mem_gpu_time.sum())/50)
+            arr_avg_total_add_host_mem_gpu_time = np.append(arr_avg_total_add_host_mem_gpu_time, avg_total_add_host_mem_gpu_time)
+            
+            avg_total_add_gpuarray_no_kernel_time = ((arr_total_add_gpuarray_no_kernel_time.sum())/50)
+            arr_avg_total_add_gpuarray_no_kernel_time = np.append(arr_avg_total_add_gpuarray_no_kernel_time, avg_total_add_gpuarray_no_kernel_time)
+            
+            avg_total_add_gpuarray_using_kernel_time = ((arr_total_add_gpuarray_using_kernel_time.sum())/50)
+            arr_avg_total_add_gpuarray_using_kernel_time = np.append(arr_avg_total_add_gpuarray_using_kernel_time, avg_total_add_gpuarray_using_kernel_time)
+            
             # Add for the rest of the methods
 
         print(current_operation + " The CPU times are")
@@ -409,12 +482,23 @@ def main():
         print(arr_avg_total_cpu_loop_time)
         # [TODO: Students should write Code]
         # Add for the rest of the methods
+        
+        print(current_operation + " The add_device_mem_gpu are")
+        print(arr_avg_total_add_device_mem_gpu_time)
+        
+        print(current_operation + " The add_host_mem_gpu are")
+        print(arr_avg_total_add_host_mem_gpu_time)
+        
+        print(current_operation + " The add_gpuarray_no_kernel are")
+        print(arr_avg_total_add_gpuarray_no_kernel_time)
+        
+        print(current_operation + " The add_gpuarray_using_kernel are")
+        print(arr_avg_total_add_gpuarray_using_kernel_time)
+        
         # Code for Plotting the results (the code for plotting can be skipped, 
         # if the student prefers to have a separate code for plotting, or to use a different software for plotting)
-        
-if __name__ == "__main__":
-    main()
-    exit()
+
+def mytest():
     size = np.int32(4)
     a = np.random.random(size).astype(np.float32)
     b = np.random.random(size).astype(np.float32)
@@ -425,3 +509,7 @@ if __name__ == "__main__":
     print(c)
     c,t = graphicscomputer.add_device_mem_gpu(a,np.float(4),size,False)
     print(c)
+
+if __name__ == "__main__":
+    # mytest()
+    main()
