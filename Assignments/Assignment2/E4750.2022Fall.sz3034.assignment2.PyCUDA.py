c2bb48861a8c980a64ca28863ae1d38ccf745462
Shuai <sz3034@columbia.edu>
2022-10-16 01:19:23 +0000

    temp checkpoint to run local

--- Assignments/Assignment2/E4750.2022Fall.sz3034.assignment2.PyCUDA.py
+++ Assignments/Assignment2/E4750.2022Fall.sz3034.assignment2.PyCUDA.py
@@ -3,6 +3,7 @@ The code in this file is part of the instructor-provided template for Assignment
 """
 
 from cProfile import label
+from cmath import isclose
 import numpy as np
 import pycuda.driver as cuda
 import pycuda.gpuarray as gpuarray
@@ -17,12 +18,13 @@ class CudaModule:
         Attributes for instance of CudaModule
         Includes kernel code and input variables.
         """
-        self.threads_per_block_x = 1024 # Students can modify this number.
+        self.threads_per_block_x = 64 # Students can modify this number.
         self.threads_per_block_y = 1
         self.threads_per_block_z = 1
         self.threads_total = self.threads_per_block_x * self.threads_per_block_y * self.threads_per_block_z
 
         self.getSourceModule()
+
 
     def getSourceModule(self):
         """
@@ -54,7 +56,7 @@ class CudaModule:
             }
             else{
                 // [TODO]: STUDENTS SHOULD WRITE CODE TO CALL THE DEVICE FUNCTION sine_taylor TO COMPUTE SINE OF INPUT VALUE
-                computed_value[n] = sinf(input_value[idx]);
+                computed_value[n] = sine_taylor(input_value[idx]);
                 #ifdef PRINT_ENABLE_DEBUG
                 if(idx<n)
                 {
@@ -75,23 +77,24 @@ class CudaModule:
         """
 
         kernel_device = """
-            #define TAYLOR_COEFFS 1000
+            #define TAYLOR_COEFFS 10000
 
-            double sine_taylor(double in) {
-                double result;           // final result
-                double term;             // untermediate term for each iter
-                double power = in;       // base case
-                double factorial = 1;    // base case
+            __device__ float sine_taylor(float in) {
+                return in;
+                float result;           // final result
+                float term;             // untermediate term for each iter
+                float power = in;       // base case
+                float factorial = 1;    // base case
                 
-                double power_iter = in * in;
-                double factorial_iter;
+                float power_iter = in * in;
+                float factorial_iter;
                 
                 for (unsigned int i = 0; i < TAYLOR_COEFFS; i++) {
                     
                     term = power / factorial;
                     
                     power = power * power_iter;
-                    factorial_iter = factorial * (i + 2) * (i + 3);
+                    factorial_iter = (i + 2) * (i + 3);
                     factorial = factorial * factorial_iter;
                     
                     if (i & 0x01)   // is odd
@@ -114,7 +117,8 @@ class CudaModule:
         # When we call this getSourceModule method for an object of this class, 
         # it will return the compiled kernelwrapper function, 
         # which will now take inputs along with block_specifications and grid_specifications.
-    
+
+
     def sine_device_mem_gpu(self, a, length, printing_properties):
         """
         Function to perform on-device parallel vector addition
@@ -140,7 +144,6 @@ class CudaModule:
         finish          = cuda.Event()
 
         # Device memory allocation for input and output arrays
-        start.record()
         a_gpu           = cuda.mem_alloc(a.size * a.dtype.itemsize)
         o_gpu           = cuda.mem_alloc(o.size * o.dtype.itemsize)
 
@@ -148,7 +151,6 @@ class CudaModule:
         malloc_start.record()
         cuda.memcpy_htod(a_gpu, a)
         cuda.memcpy_htod(o_gpu, o)
-        malloc_end.record()
 
         # Call the kernel function from the compiled module
         if(printing_properties == 'No Print'):
@@ -156,7 +158,8 @@ class CudaModule:
         elif(printing_properties == 'Print'):
             prg = self.module_with_print_nosync.get_function("main_function")
         else:
-            prg = self.module_with_print_with_sync.get_function("main_function")
+            prg = self.module_with_print_with_sync.get
+            _function("main_function")
 
         # Get grid and block dim
         blockDim  = (self.threads_per_block_x, self.threads_per_block_y, self.threads_per_block_z)
@@ -164,20 +167,15 @@ class CudaModule:
 
         # Record execution time and call the kernel loaded to the device
         # void main_function(float *input_value, float *computed_value, int n)
-        compute_start.record()
         prg(a_gpu, o_gpu, np.int32(length), block=blockDim, grid=gridDim)
 
         # Wait for the event to complete
-        compute_end.record()
-        compute_end.synchronize()
 
         # Copy result from device to the host
         cuda.memcpy_dtoh(o, o_gpu)
-        finish.record()
-        finish.synchronize()
 
         # return a tuple of output of sine computation and time taken to execute the operation.
-        return (o, [start.time_till(finish)])
+        return (o, None) # in ms
 
  
     def CPU_Sine(self, a, length, printing_properties):
@@ -192,9 +190,11 @@ class CudaModule:
         c = np.sin(a)
         end = time.time()
 
-        return c, end - start
+        return (c, (end - start) * 1000) # in ms
+
 
 def main():
+    iteration_cnt = 10
     # List all main methods
     all_main_methods = ['CPU Sine', 'Sine_device_mem_gpu']
     # List the two operations
@@ -202,7 +202,7 @@ def main():
     # List the size of vectors
     vector_sizes = 10**np.arange(1,3)
     # List iteration indexes
-    iteration_indexes = np.arange(1,50)
+    iteration_indexes = np.arange(1,iteration_cnt)
     # Select the list of valid operations for profiling
     valid_operations = all_operations
 
@@ -210,33 +210,42 @@ def main():
     graphicscomputer = CudaModule()
 
     for current_operation in valid_operations:
-        #TODO: STUDENTS CAN USE THIS SPACE TO WRITE NECESSARY TIMING ARRAYS, PERSONAL DEBUGGING PRINT STATEMENTS, ETC        
+        #TODO: STUDENTS CAN USE THIS SPACE TO WRITE NECESSARY TIMING ARRAYS, PERSONAL DEBUGGING PRINT STATEMENTS, ETC
+        cpu_time_of_each_vector_size = np.array([])
+        gpu_time_of_each_vector_size = np.array([])
+
         for vector_size in vector_sizes:
             #TODO: STUDENTS CAN USE THIS SPACE TO WRITE NECESSARY TIMING ARRAYS, PERSONAL DEBUGGING PRINT STATEMENTS, ETC
+            cpu_time_of_each_iter = np.array([])
+            gpu_time_of_each_iter = np.array([])
 
             #THE FOLLOWING VARIABLE SHOULD NOT BE CHANGED
             a_array_np = 0.001*np.arange(1,vector_size+1).astype(np.float32) #Generates an Array of Numbers 0.001, 0.002, ... 
 
             for iteration in iteration_indexes:
                 #TODO: STUDENTS CAN USE THIS SPACE TO WRITE NECESSARY TIMING ARRAYS, PERSONAL DEBUGGING PRINT STATEMENTS, ETC
-
+                cpu_answers_of_each_element = np.array([])
+                gpu_answers_of_each_element = np.array([]) # reference
                 for current_method in all_main_methods:
                     if(current_method == 'CPU Sine'):
-                        pass
                         #TODO: STUDENTS TO GET OUTPUT TIME AND COMPUTATION FROM CPU_Sine
+                        cpu_answers_of_each_element = graphicscomputer.CPU_Sine(a_array_np, None, None)
                     if(current_method == 'Sine_device_mem_gpu'):
-                        pass
                         #TODO: STUDENTS TO GET OUTPUT TIME AND COMPUTATION FROM sine_device_mem_gpu
+                        gpu_answers_of_each_element = graphicscomputer.sine_device_mem_gpu(a_array_np, a_array_np.size, current_operation)
 
-                        #TODO: STUDENTS TO COMPARE RESULTS USING ISCLOSE FUNCTION
+                #TODO: STUDENTS TO COMPARE RESULTS USING ISCLOSE FUNCTION
+                correctness = np.isclose(cpu_answers_of_each_element, gpu_answers_of_each_element)
+                        
         #TODO: STUDENTS CAN USE THIS SPACE TO WRITE NECESSARY TIMING ARRAYS, PERSONAL DEBUGGING PRINT STATEMENTS, ETC
         
 if __name__ == "__main__":
     graphicscomputer = CudaModule()
     length = 10
-    a_array_np = 0.001*np.arange(1,length+1).astype(np.float32)
+    a_array_np = 0.1*np.arange(1,length+1).astype(np.float32)
     my_answer, t0 = graphicscomputer.sine_device_mem_gpu(a_array_np, length, "Print")
     reference, t1 = graphicscomputer.CPU_Sine(a_array_np, length, "Print")
     print(a_array_np)
     print(my_answer)
     print(reference)
+    print(np.isclose(my_answer, reference))
