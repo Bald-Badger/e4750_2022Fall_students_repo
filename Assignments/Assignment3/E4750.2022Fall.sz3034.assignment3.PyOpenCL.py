ab107201f45dc077058a5959b5c7784bfaa073a8
Shuai <sz3034@columbia.edu>
2022-10-31 21:06:29 +0000

    finished?

--- Assignments/Assignment3/E4750.2022Fall.sz3034.assignment3.PyOpenCL.py
+++ Assignments/Assignment3/E4750.2022Fall.sz3034.assignment3.PyOpenCL.py
@@ -96,12 +96,6 @@ class Convolution:
 
                 // printf("Hello from global id %d x %d\n", row, col);
 
-                //#ifdef Shared_mem_optimized
-
-                // [TODO: Perform some part of Shared memory optimization routine, maybe more]
-
-                //#endif
-
                 /*
                 [TODO: Perform required tasks, mostly relating to the computation part. 
                 More #ifdef and #ifndef can be added as necessary]
@@ -129,8 +123,6 @@ class Convolution:
                         }
                     }
                 }
-                
-                
                 
                 c[row * c_cols + col] = sum;
                 __syncthreads();
@@ -174,6 +166,7 @@ class Convolution:
         
         if reverse_kernel:
             inputmask = inputmask[::-1, ::-1]
+        if debug:
             print("actual kernel:")
             print(inputmask)
             
@@ -299,9 +292,9 @@ def test_conv_pycuda(mrows = 4096, mcols = 4096, krows = 5, kcols = 5):
     K = np.random.rand(krows, kcols).astype(np.float32)
     computer = Convolution()
     cr, tr = computer.conv_cpu                          (M, K)
-    c0, t0 = computer.conv_gpu_naive                    (M, K)
-    c1, t1 = computer.conv_gpu_shared_mem               (M, K)
-    c2, t2 = computer.conv_gpu_shared_and_constant_mem  (M, K)
+    c0, t0 = computer.conv_gpu_naive                    (M, K, mrows, mcols, krows, kcols)
+    c1, t1 = computer.conv_gpu_shared_mem               (M, K, mrows, mcols, krows, kcols)
+    c2, t2 = computer.conv_gpu_shared_and_constant_mem  (M, K, mrows, mcols, krows, kcols)
     
     r0 = np.isclose(c0, cr).flatten()
     r1 = np.isclose(c1, cr).flatten()
@@ -317,7 +310,6 @@ def test_conv_pycuda(mrows = 4096, mcols = 4096, krows = 5, kcols = 5):
     for r in r2:
         if r == True:
             accuracy[2] += 1
-
     accuracy[0] /= len(r0)
     accuracy[1] /= len(r1)
     accuracy[2] /= len(r2)
@@ -325,8 +317,24 @@ def test_conv_pycuda(mrows = 4096, mcols = 4096, krows = 5, kcols = 5):
     accuracy[1] *= 100
     accuracy[2] *= 100
     
-    print(accuracy)
-    print([tr, t0, t1, t2])
+    print(
+        "testing conv2d with matrix size of {mrows} x {mcols}, kernel size of {krows} x {kcols}".format(
+            mrows=mrows, mcols=mcols, krows=krows, kcols=kcols
+        )
+    )
+    
+    print(
+        "the accuracy of the 3 kernel compared with scipy method are: {:.2f}%, {:.2f}%, {:.2f}%".format(
+            accuracy[0], accuracy[1], accuracy[2]
+        )
+    )
+    
+    print(
+        "the time it takes for scipy and each kernel to finish in μs are: {:.2f}, {:.2f}, {:.2f}, {:.2f}".format(
+            tr, t0, t1, t2
+        )
+    )
+
 
 
 def myTest():
@@ -357,9 +365,99 @@ def myTest():
     print(np.isclose(ref, sol))
 
 
+def record():
+    matrix_size_list = [16, 64, 256, 1024, 4096]
+    kernel_size = 5
+    iters = 10 # change to high value in final run
+    
+    refer_times = np.array([])
+    naive_times = np.array([])
+    share_times = np.array([])
+    const_times = np.array([])
+    
+    computer = Convolution()
+    
+    for msize in matrix_size_list:
+        refer_time = 0
+        naive_time = 0
+        share_time = 0
+        const_time = 0
+        for i in range(iters):
+            M = np.random.rand(msize, msize).astype(np.float32)
+            K = np.random.rand(kernel_size, kernel_size).astype(np.float32)
+            
+            cr, tr = computer.conv_cpu                          (M, K)
+            c0, t0 = computer.conv_gpu_naive                    (M, K, msize, msize, kernel_size, kernel_size)
+            c1, t1 = computer.conv_gpu_shared_mem               (M, K, msize, msize, kernel_size, kernel_size)
+            c2, t2 = computer.conv_gpu_shared_and_constant_mem  (M, K, msize, msize, kernel_size, kernel_size)
+            
+            refer_time += tr
+            naive_time += t0
+            share_time += t1
+            const_time += t2
+            
+            refer_time /= iters
+            naive_time /= iters
+            share_time /= iters
+            const_time /= iters
+            
+        refer_times = np.append(refer_times, refer_time)
+        naive_times = np.append(naive_times, naive_time)
+        share_times = np.append(share_times, share_time)
+        const_times = np.append(const_times, const_time)
+        filename = 'pycl_data.pkl'
+        f = open (filename,'wb')
+        pickle.dump([
+            matrix_size_list, 
+            refer_times, 
+            naive_times, 
+            share_times, 
+            const_times
+        ], f)
+        f.close()
+
+
+def plot():
+    f = open('cuda_data.pkl','rb')
+    time_list   = pickle.load(f)
+    
+    # log2 normalization
+    time_list = np.log2(time_list)
+    
+    size_list   = time_list[0]
+    refer_times = time_list[1]
+    naive_times = time_list[2]
+    share_times = time_list[3]
+    const_times = time_list[4]
+
+    
+    plt.figure(0)
+    plt.plot(size_list, refer_times, label="cpu")
+    plt.plot(size_list, naive_times, label="naive")
+    plt.plot(size_list, share_times, label="shared")
+    plt.plot(size_list, const_times, label="constant")
+    plt.legend()
+    plt.xlabel("matrix width in 2 log scale (2^x)")
+    plt.ylabel("time takes in 2 log scale (2^y μs)")
+    plt.title("PyOpenCL: time it takes for each workload in 2 log scale")
+    plt.savefig("pycl_plot_with_cpu.png")
+    
+    plt.figure(1)
+    plt.plot(size_list, naive_times, label="naive")
+    plt.plot(size_list, share_times, label="shared")
+    plt.plot(size_list, const_times, label="constant")
+    plt.legend()
+    plt.xlabel("matrix width in 2 log scale (2^x)")
+    plt.ylabel("time takes in 2 log scale (2^y μs)")
+    plt.title("PyOpenCL: time it takes for each workload in 2 log scale without CPU")
+    plt.savefig("pycl_plot_without_cpu.png")
+
+
 def main():
-    test_conv_pycuda()
     #myTest()
+    test_conv_pycuda()
+    record()
+    plot()
 
 
 if __name__ == "__main__":
