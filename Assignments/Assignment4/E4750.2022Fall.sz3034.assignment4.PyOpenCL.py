ed9af3de8840e1dee2a43f496f7cde707e0115f0
Shuai <sz3034@columbia.edu>
2022-11-07 00:42:10 +0000

    init assignment 4 code

---
+++ Assignments/Assignment4/E4750.2022Fall.sz3034.assignment4.PyOpenCL.py
@@ -0,0 +1,29 @@
+import relevant.libraries
+
+class PrefixSum:
+  def __init__(self):
+    pass
+
+  def prefix_sum_python(self):
+    # implement this, note you can change the function signature (arguments and return type)
+    pass
+
+  def prefix_sum_gpu_naive(self):
+    # implement this, note you can change the function signature (arguments and return type)
+    pass
+
+  def prefix_sum_gpu_work_efficient(self):
+    # implement this, note you can change the function signature (arguments and return type)
+    pass
+
+  def test_prefix_sum_python(self):
+    # implement this, note you can change the function signature (arguments and return type)
+    pass
+
+  def test_prefix_sum_gpu_naive(self):
+    # implement this, note you can change the function signature (arguments and return type)
+    pass
+
+  def test_prefix_sum_gpu_work_efficient(self):
+    # implement this, note you can change the function signature (arguments and return type)
+    pass
