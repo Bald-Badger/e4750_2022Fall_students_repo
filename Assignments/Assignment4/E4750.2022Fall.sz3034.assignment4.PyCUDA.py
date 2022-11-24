3e1c474b960f8d416b545e52963bcd5f45198dd5
Shuai <sz3034@columbia.edu>
2022-11-23 19:42:48 -0500

    sketchy, but checkpoint

--- Assignments/Assignment4/E4750.2022Fall.sz3034.assignment4.PyCUDA.py
+++ Assignments/Assignment4/E4750.2022Fall.sz3034.assignment4.PyCUDA.py
@@ -32,7 +32,7 @@ class PrefixSum:
             
             __syncthreads();
             
-            for (int stride = 1; stride <= SECTION_SIZE; stride <<= 1) {
+            for (unsigned int stride = 1; stride <= SECTION_SIZE; stride <<= 1) {
                 __syncthreads();
                 //if (tid == 0) printf("tid 0 reached 3 \n", tid);
                 float partial = 0;
@@ -73,11 +73,45 @@ class PrefixSum:
         """
         
         kernel_efficient    = r"""
-        
+        __global__ void partial_sum (float* in, float* out, float* step_sum, int size) {
+            const int tid = threadIdx.x;
+            const int i = blockIdx.x * blockDim.x + threadIdx.x;
+            __shared__ float XY[SECTION_SIZE * 2];
+            
+            if (i < size) {
+                XY[tid] = in[i];
+            }
+            
+            for (unsigned int stride = 1; stride <= SECTION_SIZE; stride <<=1) {
+                int index = (tid + 1) * stride * 2 - 1;
+                if (index < 2 * SECTION_SIZE) {
+                    XY[index] += XY[index - stride];
+                }
+                __syncthreads();
+            }
+            
+            for (unsigned int stride = SECTION_SIZE / 2; stride > 0; stride >>= 1) {
+                __syncthreads();
+                int index = (tid + 1) * stride * 2 - 1;
+                if (index + stride < 2 * SECTION_SIZE) {
+                    XY[index + stride] += XY[index];
+                }
+            }
+            
+            __syncthreads();
+            
+            if (i < size) {
+                out[i] = XY[tid];
+            }
+            
+            if ( (i + 1) % SECTION_SIZE == 0 ) {
+                step_sum[i / SECTION_SIZE] = out[i];
+            }
+        }
         """
 
         self.gpu_naive_module       = SourceModule(section_size + kernel_naive)
-        self.gpu_efficient_module   = SourceModule(kernel_efficient)
+        self.gpu_efficient_module   = SourceModule(section_size + kernel_efficient)
 
 
     def reference_sum(self, arr=np.ndarray):
@@ -105,16 +139,15 @@ class PrefixSum:
         return (out, (end - start) * 1000000)   #in us
 
 
-    def prefix_sum_gpu_naive(self, in_cpu=np.ndarray, seg_size=32):
-        # start = cuda.Event()
-        # compute = cuda.Event()
-        # finish = cuda.Event()
-        # print("segment size is {}".format(self.threads_per_block_x))
+    def prefix_sum_gpu_wrapper(self, in_cpu=np.ndarray, seg_size=32, scheme="naive"):
+        start = cuda.Event()
+        finish = cuda.Event()
         out = np.zeros_like(in_cpu)
         step_size = math.ceil(in_cpu.shape[0]/seg_size)
         step_cpu = np.zeros(step_size, dtype=np.float32)
-        # print(step_cpu)
-        # start.record()
+
+        start.record()
+        
         in_gpu      = cuda.mem_alloc(in_cpu.shape[0] * in_cpu.dtype.itemsize)
         out_gpu     = cuda.mem_alloc(in_cpu.shape[0] * in_cpu.dtype.itemsize)
         step_gpu    = cuda.mem_alloc(step_cpu.shape[0] * step_cpu.dtype.itemsize)
@@ -128,11 +161,17 @@ class PrefixSum:
         
         gridDim   = (math.ceil(in_cpu.shape[0]/self.threads_per_block_x), 1, 1)
         
-        print("array size is {}".format(in_cpu.shape))
-        print("block dim is ({},{},{})".format(blockDim[0], blockDim[1], blockDim[2]))
-        print("grid dim is ({},{},{})".format(gridDim[0], gridDim[1], gridDim[2]))
+        # print("array size is {}".format(in_cpu.shape))
+        # print("block dim is ({},{},{})".format(blockDim[0], blockDim[1], blockDim[2]))
+        # print("grid dim is ({},{},{})".format(gridDim[0], gridDim[1], gridDim[2]))
         
-        func = self.gpu_naive_module.get_function("partial_sum")
+        if scheme == "naive":
+            func = self.gpu_naive_module.get_function("partial_sum")
+        elif scheme == "efficient":
+            func = self.gpu_efficient_module.get_function("partial_sum")
+        else:
+            print("unknown scheme!")
+            exit()
         
         func (
             in_gpu,
@@ -142,17 +181,15 @@ class PrefixSum:
             block=blockDim,
             grid=gridDim
         )
-        print("step 1")
+
         cuda.memcpy_dtoh(step_cpu, step_gpu)
         step_cpu,t = self.prefix_sum_python(step_cpu)
         cuda.memcpy_htod(step_gpu, step_cpu)
-        #print(step_cpu)
-        print("step 2")
         
         func = self.gpu_naive_module.get_function("add_partial_sum")
-        blockDim  = (1024, 1, 1)
+        blockDim  = (self.threads_per_block_x, 1, 1)
         
-        gridDim   = (math.ceil(in_cpu.shape[0]/1024), 1, 1)
+        gridDim   = (math.ceil(in_cpu.shape[0]/self.threads_per_block_x), 1, 1)
         
         func (
             in_gpu,
@@ -163,15 +200,20 @@ class PrefixSum:
             grid=gridDim
         )
         
-        print("step 3")
-        
         cuda.memcpy_dtoh(out, out_gpu)
         
-        return (out, 0) # in us
-
-
-    def prefix_sum_gpu_work_efficient(self):
-        pass
+        finish.record()
+        finish.synchronize()
+        
+        return (out, start.time_till(finish)*1000) # in us
+    
+    
+    def prefix_sum_gpu_naive(self, in_cpu=np.ndarray, seg_size=32):
+        return self.prefix_sum_gpu_wrapper(in_cpu, seg_size, "naive")
+        
+    
+    def prefix_sum_gpu_work_efficient(self, in_cpu=np.ndarray, seg_size=32):
+        return self.prefix_sum_gpu_wrapper(in_cpu, seg_size, "efficient")
 
 
     def test_prefix_sum_python(self, arr=np.ndarray):
@@ -183,24 +225,31 @@ class PrefixSum:
         print("test_prefix_sum_python passed")
 
 
-    def test_prefix_sum_gpu_naive(self, arr=np.ndarray):
+    def test_prefix_sum_gpu_wrapper(self, arr=np.ndarray, scheme=""):
         ref, tr = self.reference_sum(arr)
-        out, to = self.prefix_sum_gpu_naive(arr, self.threads_per_block_x)
+        if scheme == "naive":
+            out, to = self.prefix_sum_gpu_naive(arr, self.threads_per_block_x)
+        elif scheme == "efficient":
+            out, to = self.prefix_sum_gpu_work_efficient(arr, self.threads_per_block_x)
         assert len(ref) == len(out)
         if (np.isclose(ref[len(ref)-1], out[len(ref)-1])):
-            print("test_prefix_sum_gpu_naive passed")
+            print("test_prefix_sum_gpu_{} passed".format(scheme))
         else:
-            print("test_prefix_sum_gpu_naive failed")
+            print("test_prefix_sum_gpu_{} failed".format(scheme))
             print("expect {}, get {}".format(out[len(ref)-1], ref[len(ref)-1]))
 
 
-    def test_prefix_sum_gpu_work_efficient(self):
-        pass
+    def test_prefix_sum_gpu_naive(self, arr=np.ndarray):
+        self.test_prefix_sum_gpu_wrapper(arr, "naive")
+
+
+    def test_prefix_sum_gpu_work_efficient(self, arr=np.ndarray):
+        self.test_prefix_sum_gpu_wrapper(arr, "efficient")
     
     
 def main():
     # init object
-    seg_len = 1024
+    seg_len = 32
     compute = PrefixSum(seg_len)
     
     # Programming Task 1. 1-D Scan - Naive Python algorithm
@@ -209,12 +258,12 @@ def main():
     compute.test_prefix_sum_python(arr1)
     compute.test_prefix_sum_python(arr2)
     
-    # Programming Task 2. 1-D Scan - Programing in PyCuda and PyOpenCL
-    arr = np.ones(4194304, dtype=np.float32)
-    # out, to = compute.prefix_sum_gpu_naive(arr, seg_len)
-    # print(arr)
-    # print(out)
+    # Programming Task 2-1 1-D Scan - Work-inefficient scan
+    arr = np.ones(134215680, dtype=np.float32)
     compute.test_prefix_sum_gpu_naive(arr)
+    
+    # Programming Task 2-2 1-D Scan - Work-efficient scan
+    compute.test_prefix_sum_gpu_work_efficient(arr)
 
 
 if __name__ == "__main__":
